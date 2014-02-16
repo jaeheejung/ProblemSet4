8ec4e3737c5aa0edb0f26d21d73910ec3d0078a5
jaeheejung <jaeheejung@wustl.edu>
2014-02-16 17:51:07 -0600

    PS4
    Includes code for Globals.R

--- R.PS4.Jung.R
+++ R.PS4.Jung.R
@@ -5,7 +5,7 @@
 Organize <- function(filename){
 #This function takes as input a generic NetLogo file.
 
-namedatetime <- scan(file=filename,what="",skip=1,nlines=2)
+namedatetime <- scan(file=filename,what="",skip=1,nlines=2,quiet=TRUE)
 #Read in the second and third lines, which contain the name of the original NetLogo file and the date and the time the simulation was run.
 
 namedatetime <-gsub(",","",namedatetime[-4])
@@ -27,7 +27,27 @@ sapply(X=c("Globals","Turtles","Plots"),FUN=function(X){dir.create(file.path(nam
 #Create 3 sub-directories named Globals, Turtles, and Plots using the sapply function.
 
 sapply(X=c("PositionPlot","WinnersPlot","PolarizationPlot","IncumbentPercentagePlot"),FUN=function(X){dir.create(file.path(namedatetime,"Plots",X))})
-#Create 4 sub-directories under the sub-directory Plots using the sapply function.	
+#Create 4 sub-directories under the sub-directory Plots using the sapply function.
+
+globals.names <- scan(file=filename,what="",sep=",",skip=8,nlines=1,quiet=TRUE)
+#Read in the 9th row, which consists of characters separated by commas.
+
+globals.values <- scan(file=filename,what=c(0,""),sep=",",skip=9,nlines=1,quiet=TRUE)
+#Read in the 10th row consisting of both numbers and characters.
+
+globals.values <- gsub("\\[|\\]","",globals.values)
+#Get rid of the square brackets.
+
+globals.values <- strsplit(globals.values," ")
+#Split the elements of globals.values, resulting in a list of 84 elements.
+
+names(globals.values) <- globals.names
+#Assign the values of globals.names as the names of the elements in globals.values, thereby creating a single combined list.
+
+globals.list <- globals.values
+#Rename the list into globals.list for the sake of clarity.
+
+dump("globals.list",file=file.path(namedatetime,"Globals","Globals.R"))
+#Dump globals.list as a file named Globals.R into the Globals sub-directory.
+
 }
-
-
